19371bc46042d89af144e9eca8e1a652aab7db30
Paradon Srita <paradonsrita2003@gmail.com>
2024-03-11 00:28:10 +0700

    put books

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const express = require('express')
-const app = express()
+
 const bodyParser = require('body-parser')
+const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
-
 const books = require('./db')
 app.get('/books', (req, res) => {
   res.json(books)
 })
-
 app.get('/books/:id', (req, res) => {
-  res.json(books.find(book => book.id === req.params.id))
+    res.json(books.find(book => book.id === req.params.id))
+})
+app.post('/books', (req, res) => {
+    books.push(req.body)
 })
 
-
-app.post('/books', (req, res) => {
-  books.push(req.body)
+app.put('/books/:id', (req, res) => {
+    const updateIndex = books.findIndex(book => book.id === req.params.id)
+    res.json(Object.assign(books[updateIndex], req.body))
 })
 
 app.listen(3000, () => {
