10d8b3e90927218a6bb16560907c7fc95002f19c
Paradon Srita <paradonsrita2003@gmail.com>
2024-03-11 00:26:18 +0700

    post books

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.get('/books/:id', (req, res) => {
   res.json(books.find(book => book.id === req.params.id))
 })
 
+
+app.post('/books', (req, res) => {
+  books.push(req.body)
+})
+
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
